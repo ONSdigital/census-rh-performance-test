b1784a7e78ddc489addbaeacb4007364d57ad652
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2020-07-30 10:35:42 +0100

    CR-922 Sanity checking instance settings. Added some comments

--- locust_tasks/setup.py
+++ locust_tasks/setup.py
@@ -28,12 +28,80 @@ def setup():
         publish_test_data()
 
     num_event_rows = get_num_event_data_records()
-    sys.stdout.write('num lines: %d\n' % num_event_rows)
     (first_record, last_record) = calculate_section_of_event_data_file(num_event_rows)            
     read_event_data(first_record, last_record)
 
 
+def get_num_event_data_records():
+    """
+    This function reads the event data file to find out how many records it holds.
+    :return: The number of records in the event data file, not including the header line.
+    """
+
+    lines = 0
+    for line in open(FILE_NAME):
+        lines += 1
+        
+    # Actual number of records is one less due to header line
+    return lines - 1
+
+    
+def calculate_section_of_event_data_file(number_records):
+    """
+    This function uses the instance settings and the number of records in the event data file to calculate the
+    section of the file which is owned by the current instance.
+    :param number_records: Is the number of entries in the event data file (ignoring the header line).
+    :return: The number of the first and last event data entries owned by the current instance. This is numbered from 0 and ignores the header line.
+    """
+
+    # Verify env.variables set
+    if INSTANCE_NUM is None:
+        sys.exit("ERROR: Environment variable 'INSTANCE_NUM' has not been set")
+    if MAX_INSTANCES is None:
+        sys.exit("ERROR: Environment variable 'MAX_INSTANCES' has not been set")
+
+    # Convert instance settings from strings
+    instance_num = int(INSTANCE_NUM)
+    max_instances = int(MAX_INSTANCES)
+    
+    # Sanity check the instance settings
+    if instance_num < 1 or instance_num > max_instances:
+        sys.stdout.write('ERROR: Invalid instance number: %d. Must be in the range 1...%d\n' % (instance_num, max_instances))
+        sys.exit(-1)
+    if max_instances < number_records:
+        sys.exit("ERROR: Event data file too small. There must be at least one worker instance per record in the file") 
+
+    # Calculate range of file owned by this instance
+    records_per_instance = number_records / max_instances
+    first_record = int(records_per_instance * (instance_num-1))
+    last_record = int(records_per_instance * (instance_num)) -1
+
+    sys.stdout.write('Instance %d/%d: Event range: %d...%d inclusive from %d records\n' % (instance_num, max_instances, first_record, last_record, number_records))
+
+    return (first_record, last_record)
+    
+
+def read_event_data(first_record, last_record):
+    """
+    Read in the section of the event data file which belongs to the current instance.
+    :param first_record: The first record from the CSV event data file to use. Numbered from 0 after the header line.
+    :param last_record: The final record from the CSV event data file that belongs to the current instance
+    """
+
+    with open(FILE_NAME, 'r') as infile:
+        reader = csv.DictReader(infile)
+        line_number = 0
+        for line in reader:
+            if line_number >= first_record and line_number <= last_record:
+                cases.append(line)
+            line_number += 1
+
+
 def publish_test_data():
+    """
+    Send all Case/UAC data from the event data file to the RH service.
+    """
+
     parameters = pika.URLParameters(RABBITMQ_URL)
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
@@ -55,49 +123,6 @@ def publish_test_data():
                                   body=case_event)
 
     connection.close()
-
-
-def get_num_event_data_records():
-    lines = 0
-    for line in open(FILE_NAME):
-        lines += 1
-        
-    # Actual number of records is one less due to header line
-    return lines - 1
-
-    
-def calculate_section_of_event_data_file(number_records):
-    # Verify env.variables set
-    if INSTANCE_NUM is None:
-        sys.exit("ERROR: Environment variable 'INSTANCE_NUM' has not been set")
-    if MAX_INSTANCES is None:
-        sys.exit("ERROR: Environment variable 'MAX_INSTANCES' has not been set")
-
-    # Convert from strings
-    instance_num = int(INSTANCE_NUM)
-    max_instances = int(MAX_INSTANCES)
-    
-    # Sanity check the instance settings
-    # PMB TODO
-
-    # Calculate range of file owned by this instance
-    records_per_instance = number_records / max_instances
-    first_record = int(records_per_instance * (instance_num-1))
-    last_record = int(records_per_instance * (instance_num)) -1
-
-    sys.stdout.write('Instance %d/%d: Event range: %d...%d inclusive from %d records\n' % (instance_num, max_instances, first_record, last_record, number_records))
-
-    return (first_record, last_record)
-    
-
-def read_event_data(first_record, last_record):
-    with open(FILE_NAME, 'r') as infile:
-        reader = csv.DictReader(infile)
-        line_number = 0
-        for line in reader:
-            if line_number >= first_record and line_number <= last_record:
-                cases.append(line)
-            line_number += 1
 
 
 def uac_event_builder(line, case_id, collection_exercise_id):
